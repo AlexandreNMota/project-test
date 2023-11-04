fcdbaea4827fcc36aa40cc481ff08c8396d13483
Alexandre <alexandre.acustica@gmail.com>
2023-11-04 15:22:44 -0300

    work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 // src/App.js
 import React from "react";
-import AppRouter from "./AppRouter";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/globals/Header/Header";
 import Nav from "./components/globals/nav/Nav";
 import Home from "./pages/Home";
+import Works from "./pages/Works";
 import "./css/main.sass";
 import "./css/main.css";
 function App() {
@@ -14,8 +14,11 @@ function App() {
         <Header />
         <Nav />
         <ul className="l-main-content main-content">
-          <li className="l-section section section--is-active">
+          <li className="l-section section ">
             <Home />
+          </li>
+          <li className="l-section section section--is-active">
+            <Works />
           </li>
         </ul>
       </div>
--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -1,4 +1,73 @@
+import victory from "../img/work-victory.jpg";
+import nowak from "../img/work-alex-nowak.jpg";
+import metiew from "../img/work-metiew-smith.jpg";
+import prev from "../components/icons/prev.svg";
+import next from "../components/icons/next.svg";
 const Works = () => {
-  return <h1>Works</h1>;
+  return (
+    <div className="work">
+      <h2>Selected work</h2>
+      <div className="work--lockup">
+        <ul className="slider">
+          <li className="slider--item slider--item-left">
+            <a href="#0">
+              <div className="slider--item-image">
+                <img src={victory} alt="Welcome" />
+              </div>
+              <p className="slider--item-title">Victory</p>
+              <p className="slider--item-description">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do.
+              </p>
+            </a>
+          </li>
+          <li className="slider--item slider--item-center">
+            <a href="#0">
+              <div className="slider--item-image">
+                <img src={nowak} alt="Welcome" />
+              </div>
+              <p className="slider--item-title">Metiew &amp; Smith</p>
+              <p className="slider--item-description">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do.
+              </p>
+            </a>
+          </li>
+          <li className="slider--item slider--item-rightslider--item slider--item-right">
+            <a href="#0">
+              <div className="slider--item-image">
+                <img src={metiew} alt="Welcome" />
+              </div>
+              <p className="slider--item-title">Alex Nowak</p>
+              <p className="slider--item-description">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do.
+              </p>
+            </a>
+          </li>
+        </ul>
+        <div className="slider--prev" style={{ color: "#fff" }}>
+          <div
+            style={{
+              width: "15px", // Defina a largura da div conforme necessário
+              height: "15px", // Defina a altura da div conforme necessário
+              backgroundImage: `url("${prev}")`, // Use a variável seta para a imagem de fundo
+              backgroundSize: "contain", // Ajuste o tamanho da imagem de acordo com suas necessidades
+              backgroundRepeat: "no-repeat",
+              color: "#fff",
+            }}
+          ></div>
+        </div>
+        <div className="slider--next" style={{ color: "#fff" }}>
+          <div
+            style={{
+              width: "15px", // Defina a largura da div conforme necessário
+              height: "15px", // Defina a altura da div conforme necessário
+              backgroundImage: `url("${next}")`, // Use a variável seta para a imagem de fundo
+              backgroundSize: "contain", // Ajuste o tamanho da imagem de acordo com suas necessidades
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Works;
